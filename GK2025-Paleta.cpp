01b697831cfd13a1e124729f01a262f47cd09e78
TheMarineLord2 <jbullmann10+github@gmail.com>
2025-04-05 08:51:17 +0200

    Initial Commit
    File prepared for Doctor teaching us the subject

---
+++ GK2025-Paleta.cpp
@@ -0,0 +1,4 @@
+// funkcje do redukcji kolorów i tworzenia palet
+#include "GK2025-Paleta.h"
+#include "GK2025-Zmienne.h"
+#include "GK2025-Funkcje.h"
---
+++ GK2025-Paleta.h
@@ -0,0 +1,11 @@
+// funkcje do redukcji kolorów i tworzenia palet
+#ifndef GK2025_PALETA_H_INCLUDED
+#define GK2025_PALETA_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#endif // GK2025_PALETA_H_INCLUDED
